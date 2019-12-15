981b2cc1ee675b3c98465d355dfb545bead62149
Bola Bally <bolabally@gmail.com>
2019-12-15 20:53:28 +0000

    add DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,10 +7,22 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      *
-     * @return void
+     *  @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+     public function run()
+     {
+        // Let's truncate our existing records to start from scratch.
+        Notebook::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few notebooks in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Notebook::create([
+                'heading' => $faker->sentence,
+                'description' => $faker->paragraph
+                'date' => $faker->sentence,          
+            ]);
+        }
     }
 }
