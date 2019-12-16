818148230a0bad791328a2c152235f3a6bb187fc
Bola Bally <bolabally@gmail.com>
2019-12-16 17:41:47 +0000

    add api.php

--- app/Http/Controllers/NotebookController.php
+++ app/Http/Controllers/NotebookController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class NotebookController extends Controller
 {
-    //
+    public function index()
+    {
+        return Notebook::all();
+    }
+ 
+    public function show($id)
+    {
+        return Notebook::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return Notebook::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $notebook = Notebook::findOrFail($id);
+        $notebook->update($request->all());
+
+        return $notebook;
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $notebook = Notebook::findOrFail($id);
+        $notebook->delete();
+
+        return 204;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,8 @@ use App\Notebook;
 |
 */
 
+Route::get('notebooks', 'NotebookController@index');
+Route::get('notebooks/{notebook}', 'NotebookController@show');
+Route::post('notebooks', 'NotebookController@store');
+Route::put('notebooks/{notebook}', 'NotebookController@update');
+Route::delete('notebooks/{notebook}', 'NotebookController@delete');
