2ec1734b19af5f83c3fd5ba6bd60b763bb7fe10d
Bola Bally <bolabally@gmail.com>
2019-12-17 20:45:14 +0000

    add UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,4 +24,13 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
+
+$factory->define(App\Notebook::class, function (Faker\Generator $faker) {
+    return [
+        'heading' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'title' => $faker->sentence,
+    ];
 });
+
+
