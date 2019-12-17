9ab9c8f31520b14c7007936311dafee79e5febb3
Bola Bally <bolabally@gmail.com>
2019-12-17 19:08:34 +0000

    add User.php

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use App\Notebook;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('register', 'Auth\RegisterController@register');
 Route::get('notebooks', 'NotebookController@index');
 Route::get('notebooks/{notebook}', 'NotebookController@show');
 Route::post('notebooks', 'NotebookController@store');
