67bd4042c8435458c9bd660aae075efdb80654b2
Bola Bally <bolabally@gmail.com>
2020-01-30 13:47:44 +0000

    addLoginControllerTest.php

---
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
